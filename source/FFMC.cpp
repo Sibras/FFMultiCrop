f8c6c0a5babc0ab849866d2c5df206536d3bb20d
Matt Oliver <protogonoi@gmail.com>
2019-07-22 23:53:53 +1000

    Add basic crop list error checking.

--- source/FFMC.cpp
+++ source/FFMC.cpp
@@ -20,6 +20,8 @@
 #include "FFFrameReader.h"
 #include "FFMultiCrop.h"
 
+#include <algorithm>
+
 extern "C" {
 #include <libavutil/frame.h>
 }
@@ -39,8 +41,17 @@ bool MultiCrop::cropAndEncode(
     // Create output encoders for each crop sequence
     vector<shared_ptr<Ffr::Encoder>> encoders;
     for (const auto& i : cropList) {
+        // Validate the input crop sequence
+        if (i.m_resolution.m_height > stream->getHeight() || i.m_resolution.m_width > stream->getWidth()) {
+            Ffr::log("Required output resolution is greater than input stream"s, Ffr::LogLevel::Error);
+            return false;
+        }
+        if (i.m_cropList.size() > static_cast<size_t>(stream->getTotalFrames())) {
+            Ffr::log("Crop list contains more frames than are found in input stream"s, Ffr::LogLevel::Error);
+            return false;
+        }
         // Create the new encoder
-        encoders.emplace_back(make_shared<Ffr::Encoder>(sourceFile, i.m_resolution.m_width, i.m_resolution.m_height,
+        encoders.emplace_back(make_shared<Ffr::Encoder>(i.m_fileName, i.m_resolution.m_width, i.m_resolution.m_height,
             Ffr::getRational(Ffr::StreamUtils::getSampleAspectRatio(stream.get())), stream->getPixelFormat(),
             Ffr::getRational(Ffr::StreamUtils::getFrameRate(stream.get())), stream->getDuration(), options.m_type,
             options.m_quality, options.m_preset, options.m_gopSize, Ffr::Encoder::ConstructorLock()));
@@ -70,12 +81,34 @@ bool MultiCrop::cropAndEncode(
             auto newFrame = Ffr::Frame(
                 copyFrame, frame->m_timeStamp, frame->m_frameNum, frame->m_formatContext, frame->m_codecContext);
 
-            // Apply crop settings
             const auto crop = cropList[current].m_cropList[newFrame.getFrameNumber()];
-            newFrame.m_frame->crop_top = crop.m_top;
-            newFrame.m_frame->crop_bottom = cropList[current].m_resolution.m_height - crop.m_top;
-            newFrame.m_frame->crop_left = crop.m_left;
-            newFrame.m_frame->crop_right = cropList[current].m_resolution.m_width - crop.m_left;
+            // Correct out of range crop values
+            auto cropTop = std::min(crop.m_top, stream->getHeight() - cropList[current].m_resolution.m_height);
+            auto cropLeft = std::min(crop.m_left, stream->getWidth() - cropList[current].m_resolution.m_width);
+            auto cropBottom = cropList[current].m_resolution.m_height + cropTop;
+            if (cropBottom > stream->getHeight()) {
+                cropTop -= cropBottom - stream->getHeight();
+                cropBottom = 0;
+            } else {
+                cropBottom = stream->getHeight() - cropBottom;
+            }
+            auto cropRight = cropList[current].m_resolution.m_width + cropLeft;
+            if (cropRight > stream->getWidth()) {
+                cropLeft -= cropRight - stream->getWidth();
+                cropRight = 0;
+            } else {
+                cropRight = stream->getWidth() - cropRight;
+            }
+            if (cropTop != crop.m_left || cropLeft != crop.m_left) {
+                Ffr::log("Out of range crop values detected, crop has been clamped for frame: "s + to_string(current),
+                    Ffr::LogLevel::Warning);
+            }
+
+            // Apply crop settings
+            newFrame.m_frame->crop_top = cropTop;
+            newFrame.m_frame->crop_bottom = cropBottom;
+            newFrame.m_frame->crop_left = cropLeft;
+            newFrame.m_frame->crop_right = cropRight;
 
             // Encode new frame
             if (!i->encodeFrame(frame)) {
