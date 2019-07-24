269b07e96fa8cf67ee39bd91160058422bdf4542
Matt Oliver <protogonoi@gmail.com>
2019-07-24 17:31:47 +1000

    Test: Update.

--- test/FFMCTest.cpp
+++ test/FFMCTest.cpp
@@ -26,10 +26,10 @@ struct TestParamsEncode
     std::vector<CropOptions> m_cropList;
 };
 
-static CropOptions s_options1 = {{{0, 0}, {0, 1}}, {60, 40}, "test-mc-1"};
+static CropOptions s_options1 = {{{0, 0}, {0, 1}}, {60, 40}, "test-mc-1.mkv"};
 
 static std::vector<TestParamsEncode> g_testDataEncode = {
-    {1, {s_options1}},
+    {0, {s_options1}},
 };
 
 class EncodeTest1 : public ::testing::TestWithParam<TestParamsEncode>
@@ -40,18 +40,41 @@ protected:
     void SetUp() override
     {
         Ffr::setLogLevel(Ffr::LogLevel::Error);
+        m_cropOps = GetParam().m_cropList;
+
+        // Generate list of crops
+        int32_t directionX = 1;
+        int32_t directionY = 1;
+        int32_t x = 0, y = 0;
+        for (auto& j : m_cropOps) {
+            for (int32_t i = 0; i < 1000; ++i) {
+                if (x >= 1920 || x < 0) {
+                    directionX *= -1;
+                    x += 2 * directionX;
+                }
+                if (y >= 1080 || y < 0) {
+                    directionY *= -1;
+                    --y;
+                }
+                j.m_cropList.emplace_back(Crop({static_cast<uint32_t>(x), static_cast<uint32_t>(y)}));
+                y += directionY;
+                x += 2 * directionX;
+            }
+        }
     }
 
     void TearDown() override {}
+
+    std::vector<CropOptions> m_cropOps;
 };
 
 TEST_P(EncodeTest1, encodeStream)
 {
     // Just run an encode and see if output is correct manually
-    ASSERT_TRUE(MultiCrop::cropAndEncode(g_testData[GetParam().m_testDataIndex].m_fileName, GetParam().m_cropList));
+    ASSERT_TRUE(MultiCrop::cropAndEncode(g_testData[GetParam().m_testDataIndex].m_fileName, m_cropOps));
 
     // Check that we can open encoded file and its parameters are correct
-    for (const auto& i : GetParam().m_cropList) {
+    for (const auto& i : m_cropOps) {
         auto stream = Ffr::Stream::getStream(i.m_fileName);
         ASSERT_NE(stream, nullptr);
 
