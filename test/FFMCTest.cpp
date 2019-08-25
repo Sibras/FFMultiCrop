99507b94cf6e559e53d7b8940407426e340c450e
Matt Oliver <protogonoi@gmail.com>
2019-08-25 21:13:27 +1000

    Test: Test multiple crop encodes.

--- test/FFMCTest.cpp
+++ test/FFMCTest.cpp
@@ -27,9 +27,10 @@ struct TestParamsEncode
 };
 
 static CropOptions s_options1 = {{{0, 0}, {0, 1}}, {640, 480}, "test-mc-1.mkv"};
+static CropOptions s_options2 = {{{0, 0}, {0, 1}}, {480, 640}, "test-mc-2.mkv"};
 
 static std::vector<TestParamsEncode> g_testDataEncode = {
-    {0, {s_options1}},
+    {0, {s_options1, s_options2}},
 };
 
 class EncodeTest1 : public ::testing::TestWithParam<TestParamsEncode>
@@ -43,26 +44,31 @@ protected:
         m_cropOps = GetParam().m_cropList;
 
         // Generate list of crops
-        int32_t directionX = 1;
-        int32_t directionY = 1;
-        int32_t x = 0, y = 0;
-        // Set width/height offset to create out of bounds values that will need to be clamped
-        const int32_t width = GetParam().m_cropList[0].m_resolution.m_width / 2;
-        const int32_t height = GetParam().m_cropList[0].m_resolution.m_height / 2;
+        int32_t xMod = 10;
+        int32_t yMod = 1;
         for (auto& j : m_cropOps) {
+            int32_t directionX = 1;
+            int32_t directionY = 1;
+            int32_t x = 0, y = 0;
+            j.m_cropList.resize(0);
+            // Set width/height offset to create out of bounds values that will need to be clamped
+            const int32_t width = j.m_resolution.m_width / 2;
+            const int32_t height = j.m_resolution.m_height / 2;
             for (int32_t i = 0; i < 1000; ++i) {
-                if (x >= (1920 - width) || x < (0 - width)) {
+                if (x >= (g_testData[GetParam().m_testDataIndex].m_width - width) || x < (0 - width)) {
                     directionX *= -1;
-                    x += 10 * directionX;
+                    x += xMod * directionX;
                 }
-                if (y >= (1080 - height) || y < (0 - height)) {
+                if (y >= (g_testData[GetParam().m_testDataIndex].m_height - height) || y < (0 - height)) {
                     directionY *= -1;
-                    --y;
+                    y += yMod * directionY;
                 }
-                j.m_cropList.emplace_back(Crop({static_cast<uint32_t>(y), static_cast<uint32_t>(x)}));
-                y += directionY;
-                x += 10 * directionX;
+                j.m_cropList.emplace_back(
+                    Crop({static_cast<uint32_t>(std::max(y, 0)), static_cast<uint32_t>(std::max(x, 0))}));
+                y += yMod * directionY;
+                x += xMod * directionX;
             }
+            std::swap(xMod, yMod);
         }
     }
 
